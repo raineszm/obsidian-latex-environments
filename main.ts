aedf0cb9c5ac196e530fae0a7ab56113d14805c2
Zach Raines <raineszm@gmail.com>
2020-11-04 20:26:14 -0500

    Add insert-new-env command
    There are still issues with losing focus after running the command.

--- main.ts
+++ main.ts
@@ -1,26 +1,30 @@
-import { App, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {App, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting} from 'obsidian';
 
-export default class MyPlugin extends Plugin {
+export default class LatexEnvironments extends Plugin {
 	onload() {
 		console.log('loading plugin');
-
-		this.addRibbonIcon('dice', 'Sample Plugin', () => {
-			new Notice('This is a notice!');
-		});
-
-		this.addStatusBarItem().setText('Status Bar Text');
-
 		this.addCommand({
-			id: 'open-sample-modal',
-			name: 'Open Sample Modal',
-			// callback: () => {
-			// 	console.log('Simple Callback');
-			// },
+			id: 'insert-new-env',
+			name: 'Insert new environment',
 			checkCallback: (checking: boolean) => {
 				let leaf = this.app.workspace.activeLeaf;
 				if (leaf) {
+					const view = leaf.view as MarkdownView;
+					const doc = view.sourceMode.cmEditor;
 					if (!checking) {
-						new SampleModal(this.app).open();
+						// new SampleModal(this.app).open();
+                        const cursor = doc.getCursor();
+                        const line = doc.getLine(cursor.line);
+                        const pos = {
+                        	line: cursor.line,
+							ch: line.length - 1
+						};
+                        const newEnvironment = '\\begin{equation}\n\n\\end{equation}\n';
+						doc.replaceRange(newEnvironment, pos);
+                        doc.setCursor({
+							line: cursor.line+2,
+							ch: 0
+						});
 					}
 					return true;
 				}
@@ -28,21 +32,12 @@ export default class MyPlugin extends Plugin {
 			}
 		});
 
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.addSettingTab(new LatexEnvironmentsSettingTab(this.app, this));
 
-		this.registerEvent(this.app.on('codemirror', (cm: CodeMirror.Editor) => {
-			console.log('codemirror', cm);
-		}));
-
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
-		console.log('unloading plugin');
+		console.log('unloading latex ennvironments');
 	}
 }
 
@@ -62,22 +57,22 @@ class SampleModal extends Modal {
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
+class LatexEnvironmentsSettingTab extends PluginSettingTab {
 	display(): void {
 		let {containerEl} = this;
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl('h2', {text: 'Settings for latex environments.'});
 
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text.setPlaceholder('Enter your secret')
-				.setValue('')
-				.onChange((value) => {
-					console.log('Secret: ' + value);
-				}));
-
+		// new Setting(containerEl)
+		// 	.setName('Setting #1')
+		// 	.setDesc('It\'s a secret')
+		// 	.addText(text => text.setPlaceholder('Enter your secret')
+		// 		.setValue('')
+		// 		.onChange((value) => {
+		// 			console.log('Secret: ' + value);
+		// 		}));
+		//
 	}
 }
