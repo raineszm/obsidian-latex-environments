1c631e09b1748581121ac7ba44ac57930d69d3da
Zach Raines <raineszm@gmail.com>
2020-11-06 11:07:13 -0500

    build(rollup): build into dist dir

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,25 @@
 import typescript from '@rollup/plugin-typescript';
 import {nodeResolve} from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import copy from "rollup-plugin-copy";
 
 export default {
-  input: 'main.ts',
-  output: {
-    dir: '.',
-    sourcemap: 'inline',
-    format: 'cjs',
-    exports: 'default'
-  },
-  external: ['obsidian'],
-  plugins: [
-    typescript(),
-    nodeResolve({browser: true}),
-    commonjs(),
-  ]
+    input: 'src/main.ts',
+    output: {
+        dir: './dist',
+        sourcemap: 'inline',
+        format: 'cjs',
+        exports: 'default'
+    },
+    external: ['obsidian'],
+    plugins: [
+        typescript(),
+        nodeResolve({browser: true}),
+        commonjs(),
+        copy({
+            targets: [
+                {src: ['manifest.json', 'styles.css'], dest: 'dist'},
+            ]
+        }),
+    ]
 };
