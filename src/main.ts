b3b962de887f826b8d7b5d4a0c56d39dafab54f4
Zach Raines <raineszm@gmail.com>
2020-11-06 12:19:51 -0500

    feat(config): add setting for defaultEnvironment

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,14 @@
-import { MarkdownView, Plugin, PluginSettingTab } from 'obsidian';
+import { App, MarkdownView, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { LatexEnvironmentsSettings } from './settings';
 
 export default class LatexEnvironments extends Plugin {
+  public settings: LatexEnvironmentsSettings;
+
   onload(): void {
+    this.loadData().then((settings) => {
+      this.settings = settings || new LatexEnvironmentsSettings();
+    });
+
     this.addCommand({
       id: 'insert-new-env',
       name: 'Insert new environment',
@@ -15,7 +22,8 @@ export default class LatexEnvironments extends Plugin {
             line: cursor.line + 1,
             ch: 0,
           };
-          const newEnvironment = '\\begin{equation}\n\n\\end{equation}\n';
+          const envName = this.settings.defaultEnvironment;
+          const newEnvironment = `\\begin{${envName}}\n\n\\end{${envName}}\n`;
           doc.replaceRange(newEnvironment, pos);
           doc.setCursor({ line: cursor.line + 2, ch: 0 });
           doc.focus();
@@ -48,6 +56,13 @@ export default class LatexEnvironments extends Plugin {
 // }
 
 class LatexEnvironmentsSettingTab extends PluginSettingTab {
+  private plugin: LatexEnvironments;
+
+  constructor(app: App, plugin: LatexEnvironments) {
+    super(app, plugin);
+    this.plugin = plugin;
+  }
+
   display(): void {
     const { containerEl } = this;
 
@@ -55,16 +70,17 @@ class LatexEnvironmentsSettingTab extends PluginSettingTab {
 
     containerEl.createEl('h2', { text: 'Settings for latex environments.' });
 
-    // new Setting(containerEl)
-    //   .setName('Default environment')
-    //   .setDesc('The default environment to insert')
-    //   .addText((text) =>
-    //     text
-    //       .setPlaceholder('environment')
-    //       .setValue('equation')
-    //       .onChange((value) => {
-    //         console.log('Secret: ' + value);
-    //       }),
-    //   );
+    new Setting(containerEl)
+      .setName('Default environment')
+      .setDesc('The default environment to insert')
+      .addText((text) =>
+        text
+          .setPlaceholder('environment')
+          .setValue(this.plugin.settings.defaultEnvironment)
+          .onChange(async (value) => {
+            this.plugin.settings.defaultEnvironment = value;
+            await this.plugin.saveData(this.plugin.settings);
+          }),
+      );
   }
 }
