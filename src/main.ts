ebd411c2b8154902bf394b1002d6064cbcd6e8a7
Zach Raines <raineszm@gmail.com>
2020-11-06 11:56:38 -0500

    fix: reset focus after inserting environments
    Before the editor would lose focus after inserting an environment. This is now fixed.

--- src/main.ts
+++ src/main.ts
@@ -1,78 +1,70 @@
-import {App, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting} from 'obsidian';
+import { MarkdownView, Plugin, PluginSettingTab } from 'obsidian';
 
 export default class LatexEnvironments extends Plugin {
-	onload() {
-		console.log('loading plugin');
-		this.addCommand({
-			id: 'insert-new-env',
-			name: 'Insert new environment',
-			checkCallback: (checking: boolean) => {
-				let leaf = this.app.workspace.activeLeaf;
-				if (leaf) {
-					const view = leaf.view as MarkdownView;
-					const doc = view.sourceMode.cmEditor;
-					if (!checking) {
-						// new SampleModal(this.app).open();
-                        const cursor = doc.getCursor();
-                        const line = doc.getLine(cursor.line);
-                        const pos = {
-                        	line: cursor.line,
-							ch: line.length - 1
-						};
-                        const newEnvironment = '\\begin{equation}\n\n\\end{equation}\n';
-						doc.replaceRange(newEnvironment, pos);
-                        doc.setCursor({
-							line: cursor.line+2,
-							ch: 0
-						});
-					}
-					return true;
-				}
-				return false;
-			}
-		});
+  onload(): void {
+    this.addCommand({
+      id: 'insert-new-env',
+      name: 'Insert new environment',
+      callback: () => {
+        const leaf = this.app.workspace.activeLeaf;
+        if (leaf) {
+          const view = leaf.view as MarkdownView;
+          const doc = view.sourceMode.cmEditor;
+          const cursor = doc.getCursor();
+          const pos = {
+            line: cursor.line + 1,
+            ch: 0,
+          };
+          const newEnvironment = '\\begin{equation}\n\n\\end{equation}\n';
+          doc.replaceRange(newEnvironment, pos);
+          doc.setCursor({ line: cursor.line + 2, ch: 0 });
+          doc.focus();
+          return true;
+        }
+        return false;
+      },
+    });
 
-		this.addSettingTab(new LatexEnvironmentsSettingTab(this.app, this));
+    this.addSettingTab(new LatexEnvironmentsSettingTab(this.app, this));
+  }
 
-	}
-
-	onunload() {
-		console.log('unloading latex ennvironments');
-	}
+  // onUnload(): void {}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		let {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		let {contentEl} = this;
-		contentEl.empty();
-	}
-}
+// class SampleModal extends Modal {
+//   constructor(app: App) {
+//     super(app);
+//   }
+//
+//   onOpen() {
+//     const { contentEl } = this;
+//     contentEl.setText('Woah!');
+//   }
+//
+//   onClose() {
+//     const { contentEl } = this;
+//     contentEl.empty();
+//   }
+// }
 
 class LatexEnvironmentsSettingTab extends PluginSettingTab {
-	display(): void {
-		let {containerEl} = this;
+  display(): void {
+    const { containerEl } = this;
 
-		containerEl.empty();
+    containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for latex environments.'});
+    containerEl.createEl('h2', { text: 'Settings for latex environments.' });
 
-		// new Setting(containerEl)
-		// 	.setName('Setting #1')
-		// 	.setDesc('It\'s a secret')
-		// 	.addText(text => text.setPlaceholder('Enter your secret')
-		// 		.setValue('')
-		// 		.onChange((value) => {
-		// 			console.log('Secret: ' + value);
-		// 		}));
-		//
-	}
+    // new Setting(containerEl)
+    //   .setName('Default environment')
+    //   .setDesc('The default environment to insert')
+    //   .addText((text) =>
+    //     text
+    //       .setPlaceholder('environment')
+    //       .setValue('equation')
+    //       .onChange((value) => {
+    //         console.log('Secret: ' + value);
+    //       }),
+    //   );
+  }
 }
