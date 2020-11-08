d942c2ca9bd48e78b71d0c23148921e5f207a62c
Zach Raines <raineszm@gmail.com>
2020-11-08 13:11:40 -0500

    test(mathblock): write tests for getEnclosingEnvironment
    Testing expected errors and successes

--- __tests__/mathblock.test.ts
+++ __tests__/mathblock.test.ts
@@ -1,5 +1,5 @@
 import { fromString } from './codemirrorHelpers';
-import { MathBlock } from '../src/mathblock';
+import { Environment, MathBlock } from '../src/mathblock';
 
 describe('MathBlock', () => {
   describe('getEnclosingEnvironment', () => {
@@ -7,6 +7,67 @@ describe('MathBlock', () => {
       const doc = fromString('$$x^2|=2$$');
       const block = new MathBlock(doc, doc.getCursor());
       expect(block.getEnclosingEnvironment(doc.getCursor())).toBeUndefined();
+    });
+
+    describe('returns the nearest enclosing environment', () => {
+      test('when in the innermost block', () => {
+        const doc = fromString(
+          `$$\\begin{equation}
+        \\begin{gathered}
+        x^2|=2\\
+        y^2=3
+        \\end{gathered}
+        \\end{equation}$$`,
+        );
+        const block = new MathBlock(doc, doc.getCursor());
+        const env = block.getEnclosingEnvironment(doc.getCursor());
+        expect(env).toBeTruthy();
+        expect((env as Environment).name).toBe('gathered');
+      });
+
+      test('when in that block contains another', () => {
+        const doc = fromString(
+          `$$\\begin{equation}|
+        \\begin{gathered}
+        x^2=2\\
+        y^2=3
+        \\end{gathered}
+        \\end{equation}$$`,
+        );
+        const block = new MathBlock(doc, doc.getCursor());
+        const env = block.getEnclosingEnvironment(doc.getCursor());
+        expect(env).toBeTruthy();
+        expect((env as Environment).name).toBe('equation');
+      });
+    });
+    describe('errors on unpaired begin or end', () => {
+      xtest('begin only for following environment', () => {
+        const doc = fromString(
+          `$$\\begin{equation}|
+        \\begin{gathered}
+        x^2=2\\
+        y^2=3
+        \\end{equation}$$`,
+        );
+        const block = new MathBlock(doc, doc.getCursor());
+        expect(() => block.getEnclosingEnvironment(doc.getCursor())).toThrow(
+          'closed',
+        );
+      });
+
+      test('begin only for enclosing environment', () => {
+        const doc = fromString(
+          `$$\\begin{equation}|
+        \\begin{gathered}
+        x^2=2\\
+        y^2=3
+        \\end{gathered}$$`,
+        );
+        const block = new MathBlock(doc, doc.getCursor());
+        expect(() => block.getEnclosingEnvironment(doc.getCursor())).toThrow(
+          'closed',
+        );
+      });
     });
   });
 });
