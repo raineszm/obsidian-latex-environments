e3067d772a37c485e5d32480e3f6cb42d82eb3bb
Zach Raines <raineszm@gmail.com>
2020-11-13 22:26:07 -0500

    feat(action): add Delete action to remove surrounding environment

--- __tests__/actions.test.ts
+++ __tests__/actions.test.ts
@@ -1,6 +1,7 @@
 import { fromString } from './codemirrorHelpers';
 import { InsertAction } from '../src/actions/insertAction';
 import { ChangeAction } from '../src/actions/changeAction';
+import { DeleteAction } from '../src/actions/deleteAction';
 
 describe('InsertAction', () => {
   it('adds environment at point', () => {
@@ -81,5 +82,46 @@ describe('ChangeAction', () => {
     action.execute('equation');
 
     expect(doc.getValue()).toBe(expected);
+  });
+});
+
+
+describe('DeleteAction', () => {
+  it('removes the begin and end of surrounding environment', () => {
+    const input = [
+      '$$\\begin{equation}',
+      '|x^2 + 1',
+      '\\end{equation}$$',
+    ].join('\n');
+
+    const expected = ['$$', 'x^2 + 1', '$$'].join('\n');
+
+    const doc = fromString(input);
+
+    const action = new DeleteAction(doc).prepare();
+    action.execute('*');
+
+    expect(doc.getValue()).toBe(expected);
+  });
+
+  it('it is a noop for no surrounding environments', () => {
+    const input = ['$$', '|x^2 + 1', '$$'].join('\n');
+    const expected = input.replace('|', '');
+
+    const doc = fromString(input);
+
+    const action = new DeleteAction(doc).prepare();
+    action.execute('*');
+
+    expect(doc.getValue()).toBe(expected);
+  });
+  it.skip('it leaves adjacent environments untouched', () => {
+
+    // const doc = fromString(input);
+
+    // const action = new DeleteAction(doc).prepare();
+    // action.execute('*');
+
+    // expect(doc.getValue()).toBe(expected);
   });
 });
