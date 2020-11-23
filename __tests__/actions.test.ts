33bffbd0c4b7895a524f67b3128cd1d47a37e04a
Zach Raines <raineszm@gmail.com>
2020-11-22 20:03:22 -0500

    test(action): test for cursor position in action tests

--- __tests__/actions.test.ts
+++ __tests__/actions.test.ts
@@ -1,4 +1,4 @@
-import { fromString } from './codemirrorHelpers';
+import { fromString, valueAndCursor } from './codemirrorHelpers';
 import { InsertAction } from '../src/actions/insertAction';
 import { ChangeAction } from '../src/actions/changeAction';
 import { DeleteAction } from '../src/actions/deleteAction';
@@ -9,17 +9,12 @@ function runAction<A extends Action>(
   input: string,
   ActionType: new (doc: CodeMirror.Doc) => A,
   envName = 'equation',
-  visualizeCursor = false,
-): string {
+): CodeMirror.Doc {
   const doc = fromString(input);
 
   const action = new ActionType(doc).prepare();
   action.execute(envName);
-  if (visualizeCursor) {
-    doc.replaceSelection('|');
-  }
-
-  return doc.getValue();
+  return doc;
 }
 
 // | indicates the location of the cursor
@@ -30,32 +25,27 @@ describe('InsertAction', () => {
     const expected = [
       '$$',
       '\\begin{equation}',
-      '',
-      '\\end{equation}',
-      '$$',
-    ].join('\n');
-    expect(runAction(input, InsertAction)).toBe(expected);
-  });
-  it('sets the cursor in the new environment', () => {
-    const input = '$$|$$';
-    const expected = [
-      '$$',
-      '\\begin{equation}',
       '|',
       '\\end{equation}',
       '$$',
     ].join('\n');
-    expect(runAction(input, InsertAction, 'equation', true)).toBe(expected);
+    const { value, cursor } = valueAndCursor(expected);
+    const doc = runAction(input, InsertAction);
+    expect(doc.getValue()).toBe(value);
+    expect(doc.getCursor()).toStrictEqual(expect.objectContaining(cursor));
   });
   it('wraps the selection with an environment', () => {
     const input = '$$|x^2 + 1|$$';
     const expected = [
       '$$\\begin{equation}',
-      'x^2 + 1',
+      'x^2 + 1|',
       '\\end{equation}$$',
     ].join('\n');
 
-    expect(runAction(input, InsertAction)).toBe(expected);
+    const { value, cursor } = valueAndCursor(expected);
+    const doc = runAction(input, InsertAction);
+    expect(doc.getValue()).toBe(value);
+    expect(doc.getCursor()).toStrictEqual(expect.objectContaining(cursor));
   });
 
   it('adds newlines to separate from adjacent content', () => {
@@ -64,12 +54,15 @@ describe('InsertAction', () => {
       '$$',
       'x^2 + 1',
       '\\begin{equation}',
-      '',
+      '|',
       '\\end{equation}',
       ' - 2',
       '$$',
     ].join('\n');
-    expect(runAction(input, InsertAction)).toBe(expected);
+    const { value, cursor } = valueAndCursor(expected);
+    const doc = runAction(input, InsertAction);
+    expect(doc.getValue()).toBe(value);
+    expect(doc.getCursor()).toStrictEqual(expect.objectContaining(cursor));
   });
   it("doesn't add a newline after when all white space after the curosr", () => {
     const input = ['$$', 'x^2 + 1|', '$$'].join('\n');
@@ -77,65 +70,83 @@ describe('InsertAction', () => {
       '$$',
       'x^2 + 1',
       '\\begin{equation}',
-      '',
+      '|',
       '\\end{equation}',
       '$$',
     ].join('\n');
-    expect(runAction(input, InsertAction)).toBe(expected);
+    const { value, cursor } = valueAndCursor(expected);
+    const doc = runAction(input, InsertAction);
+    expect(doc.getValue()).toBe(value);
+    expect(doc.getCursor()).toStrictEqual(expect.objectContaining(cursor));
   });
   it("doesn't add a newline before when all white space before the curosr", () => {
     const input = ['$$', '|x^2 + 1', '$$'].join('\n');
     const expected = [
       '$$',
       '\\begin{equation}',
-      '',
+      '|',
       '\\end{equation}',
       'x^2 + 1',
       '$$',
     ].join('\n');
-    expect(runAction(input, InsertAction)).toBe(expected);
+    const { value, cursor } = valueAndCursor(expected);
+    const doc = runAction(input, InsertAction);
+    expect(doc.getValue()).toBe(value);
+    expect(doc.getCursor()).toStrictEqual(expect.objectContaining(cursor));
   });
   it("doesn't add newlines if insert line is blank", () => {
     const input = ['$$', '|', '$$'].join('\n');
     const expected = [
       '$$',
       '\\begin{equation}',
-      '',
+      '|',
       '\\end{equation}',
       '$$',
     ].join('\n');
-    expect(runAction(input, InsertAction)).toBe(expected);
+    const { value, cursor } = valueAndCursor(expected);
+    const doc = runAction(input, InsertAction);
+    expect(doc.getValue()).toBe(value);
+    expect(doc.getCursor()).toStrictEqual(expect.objectContaining(cursor));
   });
 });
 describe('ChangeAction', () => {
   it('changes the name of surrounding environment', () => {
     const input = ['$$\\begin{equation}', '|', '\\end{equation}$$'].join('\n');
-    const expected = ['$$\\begin{multline}', '', '\\end{multline}$$'].join(
+    const expected = ['$$\\begin{multline}', '|', '\\end{multline}$$'].join(
       '\n',
     );
-    expect(runAction(input, ChangeAction, 'multline')).toBe(expected);
+    const { value, cursor } = valueAndCursor(expected);
+    const doc = runAction(input, ChangeAction, 'multline');
+    expect(doc.getValue()).toBe(value);
+    expect(doc.getCursor()).toStrictEqual(expect.objectContaining(cursor));
   });
 
   it('when no surrounding environment wraps the whole block', () => {
     const input = '$$|x^2 + 1$$';
     const expected = [
       '$$\\begin{equation}',
-      'x^2 + 1',
+      '|x^2 + 1',
       '\\end{equation}$$',
     ].join('\n');
 
-    expect(runAction(input, ChangeAction)).toBe(expected);
+    const { value, cursor } = valueAndCursor(expected);
+    const doc = runAction(input, ChangeAction);
+    expect(doc.getValue()).toBe(value);
+    expect(doc.getCursor()).toStrictEqual(expect.objectContaining(cursor));
   });
 
   it("doesn't add whitespace when no enclosing environment", () => {
     const input = ['$$', '|x^2 + 1', '$$'].join('\n');
     const expected = [
       '$$\\begin{equation}',
-      'x^2 + 1',
+      '|x^2 + 1',
       '\\end{equation}$$',
     ].join('\n');
 
-    expect(runAction(input, ChangeAction)).toBe(expected);
+    const { value, cursor } = valueAndCursor(expected);
+    const doc = runAction(input, ChangeAction);
+    expect(doc.getValue()).toBe(value);
+    expect(doc.getCursor()).toStrictEqual(expect.objectContaining(cursor));
   });
 });
 
@@ -145,15 +156,22 @@ describe('DeleteAction', () => {
       '\n',
     );
 
-    const expected = ['$$', 'x^2 + 1', '$$'].join('\n');
-    expect(runAction(input, DeleteAction)).toBe(expected);
+    const expected = ['$$', '|x^2 + 1', '$$'].join('\n');
+
+    const { value, cursor } = valueAndCursor(expected);
+    const doc = runAction(input, DeleteAction);
+    expect(doc.getValue()).toBe(value);
+    expect(doc.getCursor()).toStrictEqual(expect.objectContaining(cursor));
   });
 
   it('is a noop for no surrounding environments', () => {
     const input = ['$$', '|x^2 + 1', '$$'].join('\n');
-    const expected = input.replace('|', '');
+    const expected = input;
 
-    expect(runAction(input, DeleteAction)).toBe(expected);
+    const { value, cursor } = valueAndCursor(expected);
+    const doc = runAction(input, DeleteAction);
+    expect(doc.getValue()).toBe(value);
+    expect(doc.getCursor()).toStrictEqual(expect.objectContaining(cursor));
   });
   it('leaves adjacent environments untouched', () => {
     const input = [
@@ -167,7 +185,7 @@ describe('DeleteAction', () => {
     ].join('\n');
     const expected = [
       '$$',
-      'x^2=2\\\\',
+      'x^2|=2\\\\',
       '\\begin{pmatrix}',
       '1&2\\\\',
       '3&4',
@@ -175,7 +193,10 @@ describe('DeleteAction', () => {
       '$$',
     ].join('\n');
 
-    expect(runAction(input, DeleteAction)).toBe(expected);
+    const { value, cursor } = valueAndCursor(expected);
+    const doc = runAction(input, DeleteAction);
+    expect(doc.getValue()).toBe(value);
+    expect(doc.getCursor()).toStrictEqual(expect.objectContaining(cursor));
   });
   it('only deletes nearest enclosing environment', () => {
     const input = [
@@ -189,12 +210,15 @@ describe('DeleteAction', () => {
     const expected = [
       '$$\\begin{equation}',
       '',
-      'x^2=2\\\\',
+      'x^2|=2\\\\',
       'y^2=3',
       '',
       '\\end{equation}$$',
     ].join('\n');
 
-    expect(runAction(input, DeleteAction)).toBe(expected);
+    const { value, cursor } = valueAndCursor(expected);
+    const doc = runAction(input, DeleteAction);
+    expect(doc.getValue()).toBe(value);
+    expect(doc.getCursor()).toStrictEqual(expect.objectContaining(cursor));
   });
 });
--- __tests__/codemirrorHelpers.ts
+++ __tests__/codemirrorHelpers.ts
@@ -14,3 +14,32 @@ export function fromString(template: string): CodeMirror.Doc {
   }
   return doc;
 }
+
+export function allCursorsFromString(str: string): CodeMirror.Position[] {
+  const cursors = [];
+  const lines = str.split('\n');
+  for (let i = 0; i < lines.length; i++) {
+    const ch = lines[i].indexOf('|');
+    if (ch !== -1) {
+      cursors.push({ ch: ch, line: i });
+    }
+  }
+  return cursors;
+}
+
+export function cursorFromString(str: string): CodeMirror.Position {
+  const cursor = allCursorsFromString(str).shift();
+  if (cursor === undefined) {
+    return { ch: -1, line: -1 };
+  }
+  return cursor;
+}
+
+export function valueAndCursor(
+  str: string,
+): { value: string; cursor: CodeMirror.Position } {
+  return {
+    value: str.replace('|', ''),
+    cursor: cursorFromString(str),
+  };
+}
