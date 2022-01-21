170f62154651cac122d177424de5c27ceb5cd2ce
Zach Raines <zach@zmraines.com>
2022-01-21 10:10:55 -0500

    refactor(search): change search implementation to work with offsets
    WIP

--- src/search.ts
+++ src/search.ts
@@ -1,14 +1,12 @@
-import { Editor, EditorPosition } from 'obsidian';
-
 export class SearchCursor {
   public readonly regex: RegExp;
-  private _from: EditorPosition;
-  private _to: EditorPosition;
+  private _from: number;
+  private _to: number;
 
   constructor(
-    public doc: Editor,
+    public text: string,
     regex: RegExp | String,
-    public readonly caret: EditorPosition,
+    public readonly caret: number,
   ) {
     if (regex instanceof RegExp) {
       this.regex = regex;
@@ -20,41 +18,59 @@ export class SearchCursor {
   }
 
   public findNext(): boolean {
-    const text = this.doc.getValue().slice(this.caretOffset());
+    const text = this.text.slice(this.caret);
     const match = text.match(this.regex);
     if (match?.index == null) {
       return false;
     }
-    this._from = this.doc.offsetToPos(this.caretOffset() + match.index);
-    this._to = this.doc.offsetToPos(
-      this.caretOffset() + match.index + match[0].length,
-    );
+    this._from = this.caret + match.index;
+    this._to = this.caret + match.index + match[0].length;
     return true;
   }
 
   public findPrevious(): boolean {
     const reverseRegex = new RegExp(this.regex.source, this.regex.flags + 'g');
-    const text = this.doc.getValue().slice(this.caretOffset());
-    const matches = Array.from(text.matchAll(reverseRegex));
-    const lastMatch = matches[matches.length - 1];
-    if (lastMatch?.index == null) {
+    const text = this.text.slice(this.caret);
+    const lastMatch = getLastMatch(reverseRegex, text);
+    if (lastMatch == null) {
       return false;
     }
-    const offset = lastMatch.index;
-    this._to = this.doc.offsetToPos(offset + 1 + 1);
-    this._to = this.doc.offsetToPos(offset + lastMatch[0].length + 1);
+    this._from = lastMatch.index + 1 + 1;
+    this._to = lastMatch.index + lastMatch.text.length + 1;
     return true;
   }
 
-  public to(): EditorPosition {
+  public to(): number {
     return this._to;
   }
 
-  public from(): EditorPosition {
+  public from(): number {
     return this._from;
   }
+}
 
-  private caretOffset(): number {
-    return this.doc.posToOffset(this.caret);
+interface Match {
+  index: number;
+  text: string;
+}
+
+function getLastMatch(r: RegExp, s: string): Match | undefined {
+  const matches = Array.from(matchAll(r, s));
+  const lastMatch = matches[matches.length - 1];
+  if (lastMatch == null) {
+    return undefined;
+  } else {
+    return {
+      index: lastMatch.index,
+      text: lastMatch[0],
+    };
+  }
+}
+
+function* matchAll(r: RegExp, s: string): Generator<RegExpExecArray> {
+  while (true) {
+    const match = r.exec(s);
+    if (match == null) return;
+    yield match;
   }
 }
